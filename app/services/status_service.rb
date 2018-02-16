204a59f6c96479a697b1fa486a1b8a6e3939721b
Andrii Novikov <andruha2921@gmail.com>
2018-02-16 11:53:52 +0200

    url set

--- app/services/status_service.rb
+++ app/services/status_service.rb
@@ -1,10 +1,10 @@
 class StatusService < Rectify::Command
   def call
     {
-      id: BlockchainWorkshop::Application.secrets[:node_id],
+      id: BlockchainWorkshop::Application.secrets[:node_id].to_s,
       name: BlockchainWorkshop::Application.secrets[:name],
       last_hash: Block.last_block_hash,
-      neighbours: Node.pluck(:node_id),
+      neighbours: Node.pluck(:node_id).map(&:to_s),
       url: BlockchainWorkshop::Application.secrets[:url]
     }
   end
