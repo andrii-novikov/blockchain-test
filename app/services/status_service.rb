7c633661c0287195c0ef3e95e145530ac0d06624
Andrii Novikov <andruha2921@gmail.com>
2018-02-16 17:56:42 +0200

    fix status

--- app/services/status_service.rb
+++ app/services/status_service.rb
@@ -4,7 +4,7 @@ class StatusService < Rectify::Command
       id: BlockchainWorkshop::Application.secrets[:node_id].to_s,
       name: BlockchainWorkshop::Application.secrets[:name],
       last_hash: Block.last_block_hash,
-      neighbours: Node.pluck(:node_id).map(&:to_s),
+      neighbours: ActiveModelSerializers::SerializableResource.new(Node.all).as_json,
       url: BlockchainWorkshop::Application.secrets[:url]
     }
   end
